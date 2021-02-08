6fdfa0cbf823dc591881cc4c7faff8913dc0d9a6
manidha <77302114+manidha@users.noreply.github.com>
2021-02-07 23:14:22 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+﻿c0= int(input('Enter number:\n'))
+count = 0
+while c0 !=1:
+    if c0% 2 == 0:
+        c0 = c0 //2
+        print(c0)
+        count += 1
+    elif c0 % 2 == 1:
+        c0=  3 * c0 + 1
+        print(c0)
+        count +=1
+    
+print("steps=",count)
