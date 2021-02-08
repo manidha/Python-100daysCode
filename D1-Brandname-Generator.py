a11c2f84396b82ce603cd294f32db69c017c5aa6
manidha <77302114+manidha@users.noreply.github.com>
2021-02-07 23:45:51 -0500

    Create D1-Brandname-Generator.py

---
+++ D1-Brandname-Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+name = input("What's name?\n")
+item = input("What's your favorite item  name?\n")
+print("Your band name could be " + name + " " + item
